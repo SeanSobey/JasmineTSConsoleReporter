2e2315471b1b2983a3aefde41f1979459f664db4
Sean Sobey SAHL <seans@sahomeloans.com>
2017-09-22 15:33:15 +0200

    Added error graceful fail.

--- ts-stack-filter.js
+++ ts-stack-filter.js
@@ -8,44 +8,47 @@ const errorStackParser = require('error-stack-parser'); //https://github.com/sta
 function create(jasmineCorePath) {
 
 	return function tsStackFilter(stack) {
-		
-			if (!stack) {
-				return '';
-			}
-			return errorStackParser.parse({
-					stack: stack
-				})
-				.filter(function (stackFrame) {
-					return stackFrame.fileName.indexOf(jasmineCorePath) === -1;
-				})
-				.map((stackFrame) => {
-		
-					//console.log('stackFrame', stackFrame);
-					if (!path.isAbsolute(stackFrame.fileName)) {
-						return stackFrame.source;
-					}
-					const code = fs.readFileSync(stackFrame.fileName, 'utf8');
-					const srcMapResult = sourceMapResolve.resolveSourceMapSync(code, stackFrame.fileName, fs.readFileSync);
-					if (!srcMapResult) {
-						return stackFrame.source;
-					}
-					//console.log('stackFrame', stackFrame);
-					//console.log('srcMapResult', srcMapResult);
-					const smc = new sourceMap.SourceMapConsumer(srcMapResult.map);
-					const originalPosition = smc.originalPositionFor({
-						line: stackFrame.lineNumber,
-						column: stackFrame.columnNumber
-					});
-					//console.log('originalPosition', originalPosition);
-					//const fileName = path.resolve(originalPosition.source, srcMapResult.sourcesRelativeTo);
-					const fileName = path.normalize(stackFrame.fileName + '/../' + originalPosition.source); //TODO: handle paths not same as map file
-					//console.log('result', fileName + ':' + originalPosition.line + ':' + originalPosition.column);
-					return stackFrame.source.replace(
-						stackFrame.fileName + ':' + stackFrame.lineNumber + ':' + stackFrame.columnNumber,
-						fileName + ':' + originalPosition.line + ':' + originalPosition.column
-					);
-				}).join('\n');
+
+		if (!stack) {
+			return '';
 		}
+		return errorStackParser.parse({
+			stack: stack
+		})
+			.filter(function (stackFrame) {
+				return stackFrame.fileName.indexOf(jasmineCorePath) === -1;
+			})
+			.map((stackFrame) => {
+				//console.log('stackFrame', stackFrame);
+				if (!path.isAbsolute(stackFrame.fileName)) {
+					return stackFrame.source;
+				}
+				const code = fs.readFileSync(stackFrame.fileName, 'utf8');
+				const srcMapResult = sourceMapResolve.resolveSourceMapSync(code, stackFrame.fileName, fs.readFileSync);
+				if (!srcMapResult) {
+					return stackFrame.source;
+				}
+				//console.log('stackFrame', stackFrame);
+				//console.log('srcMapResult', Object.assign({}, srcMapResult, { map: 'excluded' })); 
+				const srcMapConsumer = new sourceMap.SourceMapConsumer(srcMapResult.map);
+				const originalPosition = srcMapConsumer.originalPositionFor({
+					line: stackFrame.lineNumber,
+					column: stackFrame.columnNumber
+				});
+				//console.log('originalPosition', originalPosition);
+				//const fileName = path.resolve(originalPosition.source, srcMapResult.sourcesRelativeTo);
+				if (!originalPosition.source) {
+					//console.log('Failed to resolve original position');
+					return stackFrame.source;
+				}
+				const fileName = path.normalize(stackFrame.fileName + '/../' + originalPosition.source); //TODO: handle paths not same as map file
+				//console.log('result', fileName + ':' + originalPosition.line + ':' + originalPosition.column);
+				return stackFrame.source.replace(
+					stackFrame.fileName + ':' + stackFrame.lineNumber + ':' + stackFrame.columnNumber,
+					fileName + ':' + originalPosition.line + ':' + originalPosition.column
+				);
+			}).join('\n');
+	}
 }
 
 module.exports = exports = {
